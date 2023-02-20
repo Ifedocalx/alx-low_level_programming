6e53ab968aed65e9c7d27f0381463e3013e7563d
Ifedocalx <adedejiifeoluwa21@gmail.com>
2023-02-20 13:02:27 -0800

    hello world

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main- print a line of code using puts
+ *Return:0
+*/int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+	printf("Size of a char:%lu byte(s)\n",(unsigned long)size of (a))
+	printf("Size of a char:%lu byte(s)\n",(unsigned long)size of (b));
+	printf("Size of a char:%lu byte(s)\n",(unsigned long)size of (c));
+        printf("Size of a char:%lu byte(s)\n",(unsigned long)size of (d));
+	printf("Size of a char:%lu byte(s)\n",(unsigned long) size of (f));
+	return (0);
+}
