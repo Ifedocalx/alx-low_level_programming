c01a303cbc28a5e5ae4bfafa26626e28ae670455
Ifedocalx <adedejiifeoluwa21@gmail.com>
2023-02-20 11:06:23 -0800

    hello world

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main- print a line of code using puts
+*Return:0
+*/
+int main(void)
+{
+	printf("with  proper grammar, but the outcome is a piece of art");
+
+	return (0);
+}
