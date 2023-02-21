c2380fbc6659bef7e04707c994e840da382c77c5
Ifedocalx <adedejiifeoluwa21@gmail.com>
2023-02-21 09:44:15 -0800

    hello world

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,17 +3,15 @@
  *main - print _putchar
  *is written by love
  *Return:0
-*/
-int min(void)
-{
-_putchar ('_');
-_putchar ('p');
-_putchar ('u');
-_putchar ('t');
-_putchar ('c');
-_putchar ('h');
-_putchar ('a');
-_putchar ('r');
-_putchar ('\n');
-return (0);
-}
+ */
+int main(void)
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
