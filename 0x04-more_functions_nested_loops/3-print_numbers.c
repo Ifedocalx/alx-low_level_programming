722796b392c98ff5a5f7bbcea2a7672f98db542e
Ifedocalx <adedejiifeoluwa21@gmail.com>
2023-02-23 17:23:15 -0800

    hello world

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * print_numbers - print num from 0-
+ * print_numbers - print num from 0-9
  * Return: void
  */
-void print_numbers(void)
+void print_numbers(void)	
 {
 	char c;
 
