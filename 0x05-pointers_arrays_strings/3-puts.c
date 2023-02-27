4a2e397e7f0b59acec7ddc6d838f6a7e269a4952
Ifedocalx <adedejiifeoluwa21@gmail.com>
2023-02-27 12:24:47 -0800

    hello world

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _puts - print a string to yhe standard output
+ * @str: the string
+ * Return: 0
+ */
+
+void _puts(char *str)
+{
+	int a = 0;
+
+	while (str[a] != '\0')
+	{
+		_putchar(str[a]);
+		a++;
+	}
+	_putchar('\n');
+}
