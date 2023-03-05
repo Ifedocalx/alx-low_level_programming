1bb784c4e4cf747d919d5750ff6f6514584a9e50
Ifedocalx <adedejiifeoluwa21@gmail.com>
2023-03-05 04:06:08 -0800

    hello world

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_sign - determines the input number
+ * print_sign - prints the sign of a number
  * @n: the imput number as an integer
  * Return: 1 is greater than zero, 0 is zero, -1 is less than zero
  */
