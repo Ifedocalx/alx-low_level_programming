1edc454fe7541019680c8d908fe94eb1b0e23d72
Ifedocalx <adedejiifeoluwa21@gmail.com>
2023-03-06 12:03:39 -0800

    hello world

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -7,5 +7,5 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void print _chessboard(char (*a)[8]);
-
+int _putchar(char c);
 #endif
