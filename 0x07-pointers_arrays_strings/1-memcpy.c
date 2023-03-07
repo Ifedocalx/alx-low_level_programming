f6929438eee51b37ffdba0eeeda7e85d48db2779
Ifedocalx <adedejiifeoluwa21@gmail.com>
2023-03-07 15:14:43 -0800

    hello world

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -2,7 +2,7 @@
 /**
  * _memcpy - The _memcpy() function copies n bytes
  * from memory area src to memory area dest
- * @dest:area where bytes are copied to
+ * @dest: area where bytes are copied to
  * @src: area where bytes are copied from
  * @n: number of bytes to copy
  * Return: returns a pointer to n
